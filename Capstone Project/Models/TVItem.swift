bbe097ee5fd5b9865ee20844d221684a18d4cd4b
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-03-12 20:40:42 +0300

    added models

---
+++ Capstone Project/Models/TVItem.swift
@@ -0,0 +1,8 @@
+//
+//  TVItem.swift
+//  Capstone Project
+//
+//  Created by Bartuğ Kaan Çelebi on 12.03.2024.
+//
+
+import Foundation
---
+++ Capstone Project/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Capstone Project
+//
+//  Created by Bartuğ Kaan Çelebi on 12.03.2024.
+//
+
+import Foundation
+
+struct User: Codable {
+  let id: String
+  let fullName: String
+  let email : String
+  let joined: TimeInterval
+  
+}
