8da6d36eb8b31f51aa5ea42e059af2703c715bf6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-03-12 20:56:17 +0300

    added logo and colorset

---
+++ Capstone Project/Views/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  Capstone Project
+//
+//  Created by Bartuğ Kaan Çelebi on 12.03.2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
