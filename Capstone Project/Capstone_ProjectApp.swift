879c44251d29a305b8d45fd1163a2990bfc1305c
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-03-12 19:25:30 +0300

    Initial Commit

---
+++ Capstone Project/Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_ProjectApp.swift
+//  Capstone Project
+//
+//  Created by Bartuğ Kaan Çelebi on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
