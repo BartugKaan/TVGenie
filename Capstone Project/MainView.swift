5b154d3175da53696d8709021b0130da76dcc9b6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-03-12 19:31:50 +0300

    changed contentViews name

--- Capstone Project/MainView.swift
+++ Capstone Project/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  Capstone Project
 //
 //  Created by Bartuğ Kaan Çelebi on 12.03.2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
