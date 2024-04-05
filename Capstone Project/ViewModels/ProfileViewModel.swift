3fc0cb6cd3b8822cbcde521ded326d47f43ffbd2
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-04-05 16:43:10 +0300

    added new profile UI design

--- Capstone Project/ViewModels/ProfileViewModel.swift
+++ Capstone Project/ViewModels/ProfileViewModel.swift
@@ -10,39 +10,56 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class ProfileViewModel: ObservableObject {
-  @Published var user: User? = nil
-  
-  init() {
+    @Published var user: User? = nil
+    @Published var errorMessage = ""
     
-  }
-  
-  func fetchUser() {
-    guard let userId = Auth.auth().currentUser?.uid else {
-      return
+    init() {
+        
     }
     
-    let db = Firestore.firestore()
-    db.collection("users")
-      .document(userId).getDocument { [weak self] snapshot, error in
-        guard let data = snapshot?.data(), error == nil else {
-          return
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
         }
-        DispatchQueue.main.async {
-          self?.user = User(
-            id: data ["id"] as? String ?? "",
-            name: data["name"] as? String ?? "",
-            email: data["email"] as? String ?? "",
-            joined: data["joined"] as? TimeInterval ?? 0)
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId).getDocument { [weak self] snapshot, error in
+                guard let data = snapshot?.data(), error == nil else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.user = User(
+                        id: data ["id"] as? String ?? "",
+                        name: data["name"] as? String ?? "",
+                        email: data["email"] as? String ?? "",
+                        joined: data["joined"] as? TimeInterval ?? 0)
+                }
+            }
+    }
+    
+    func logout(){
+        do {
+            try Auth.auth().signOut()
         }
-      }
-  }
-  
-  func logout(){
-    do {
-      try Auth.auth().signOut()
+        catch{
+            print(error)
+        }
     }
-    catch{
-      print(error)
+    
+    func deleteAccount() {
+        let currentUser = Auth.auth().currentUser
+        
+        currentUser?.delete(completion: { error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+            }
+            else {
+                self.errorMessage = "Your Account is deleted."
+            }
+        })
+        
     }
-  }
+    
+    
 }
