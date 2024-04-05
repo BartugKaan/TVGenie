984e7570e959d0aee89dd6729fb4a962d91a9d30
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-04-05 16:43:47 +0300

    update profile page UI and functions

--- Capstone Project/Views/ProfileView.swift
+++ Capstone Project/Views/ProfileView.swift
@@ -9,75 +9,92 @@ import SwiftUI
 import SplineRuntime
 
 struct ProfileView: View {
-  @StateObject var viewModel = ProfileViewModel()
-  var body: some View {
-    NavigationStack {
-      VStack {
-        
-        if let user = viewModel.user{
-          profile(user: user)
-        } else {
-          VStack {
-            Text("Profile loading...")
-              .font(.title)
-              .fontWeight(.bold)
-            
-            ProgressView()
-              .padding(.bottom, 25)
-          }
-          .padding(.top, 30)
-          Spacer()
+    @StateObject var viewModel = ProfileViewModel()
+    @State var isDeletedPressed = false
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                if let user = viewModel.user{
+                    profile(user: user,viewModel: viewModel)
+                } else {
+                    VStack {
+                        Text("Profile loading...")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        ProgressView()
+                            .padding(.bottom, 25)
+                    }
+                    .padding(.top, 30)
+                    Spacer()
+                }
+                Button(action: {
+                    isDeletedPressed = true
+                }, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(Color.red)
+                        Text("Delete this account")
+                            .foregroundStyle(Color.white)
+                    }
+                })
+                .frame(width: 300,height: 50)
+                .padding(.bottom, 32)
+            }
+            .navigationTitle("Profile")
         }
-        BigButton(title: "Sign-out") {
-          viewModel.logout()
+        .alert(isPresented: $isDeletedPressed){
+            Alert(title: Text("We will miss you 🙁"), message: Text("You cannot undo this action. Are you sure ?"),  primaryButton: .cancel(Text("Cancel")), secondaryButton: .destructive(Text("Delete"), action: {
+                viewModel.deleteAccount()
+            }))
         }
-        .padding(.bottom, 10)
-      }
-      .navigationTitle("Profile")
+        .onAppear{
+            viewModel.fetchUser()
+        }
     }
-    .onAppear{
-      viewModel.fetchUser()
-    }
-  }
 }
 
 #Preview {
-  ProfileView()
+    ProfileView()
 }
 
 @ViewBuilder
-func profile(user: User) -> some View{
+func profile(user: User, viewModel: ProfileViewModel) -> some View{
     VStack{
-    HStack {
-      Text("User Name :")
-        .font(.title2)
-      Text(user.name)
+        Text(user.name)
+            .padding(.all, 10)
+            .font(.title)
+        Form{
+            HStack {
+                Text("Email :")
+                    .font(.title2)
+                Text(user.email)
+            }
+            .padding(.all, 10)
+            HStack {
+                Text("Joined Date :")
+                    .font(.title2)
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .omitted))")
+            }
+            .padding(.all, 10)
+            HStack {
+                Button(action: {
+                    viewModel.logout()
+                }, label: {
+                    Text("Sign out")
+                        .foregroundStyle(Color.red)
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .padding()
+                })
+            }
+        }
+        .cornerRadius(25)
+        .frame(height: 270)
+        .padding(.horizontal, 16)
+        .scrollDisabled(true)
+        
+        Spacer()
     }
-    .padding(.all, 10)
-    HStack {
-      Text("Email :")
-        .font(.title2)
-      Text(user.email)
-    }
-    .padding(.all, 10)
-    HStack {
-      Text("Joined Date :")
-        .font(.title2)
-      Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
-    }
-    .padding(.all, 10)
-  }
-  Spacer()
-}
-
-struct ProfileImageView: View {
-  var body: some View {
-    // fetching from cloud
-    // let url = URL(string: "https://build.spline.design/UN6GDwWbZhVb429-EjT5/scene.splineswift")!
-
-    // // fetching from local
-    let url = Bundle.main.url(forResource: "avatar_copy", withExtension: "splineswift")!
-
-    try? SplineView(sceneFileURL: url).ignoresSafeArea(.all)
-  }
 }
