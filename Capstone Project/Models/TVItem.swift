380fa02dfcf3f85ff1601b7822792c5af9c624a6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-05-13 09:25:09 +0300

    added TVItem variables

--- Capstone Project/Models/TVItem.swift
+++ Capstone Project/Models/TVItem.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class TVItem {
+    let name  = ""
+    let brand  = ""
+    let warranty = ""
+    let hdmiCount = ""
+    let usbCount = ""
+    let screenSize = ""
+    let audioPower = ""
+    let refrshRate = ""
+    let screenResolution = ""
+    let powerConsumption = ""
+    let cost = ""
+    let cpuCoreCount = ""
+    let wifiSpecs = ""
+    let screenType = ""
+    let isSmart = ""
+    let hdmiType = ""
+    let ciScore = ""
+    let rank = ""
+    let imageUrl = ""
+}
