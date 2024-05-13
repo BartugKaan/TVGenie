e479684b5c817c886865a02f5a82ca6a9ad14ed4
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-05-13 10:26:54 +0300

    added init for TVITem

--- Capstone Project/Models/TVItem.swift
+++ Capstone Project/Models/TVItem.swift
@@ -8,23 +8,48 @@
 import Foundation
 
 class TVItem {
-    let name  = ""
-    let brand  = ""
-    let warranty = ""
-    let hdmiCount = ""
-    let usbCount = ""
-    let screenSize = ""
-    let audioPower = ""
-    let refrshRate = ""
-    let screenResolution = ""
-    let powerConsumption = ""
-    let cost = ""
-    let cpuCoreCount = ""
-    let wifiSpecs = ""
-    let screenType = ""
-    let isSmart = ""
-    let hdmiType = ""
-    let ciScore = ""
-    let rank = ""
-    let imageUrl = ""
+    let name : String
+    let brand : String
+    let warranty : String
+    let hdmiCount : String
+    let usbCount : String
+    let screenSize : String
+    let audioPower : String
+    let refrshRate : String
+    let screenResolution : String
+    let powerConsumption : String
+    let cost : Int
+    let cpuCoreCount : String
+    let wifiSpecs : String
+    let screenType : String
+    let isSmart : String
+    let hdmiType : String
+    let ciScore : String
+    let rank : Int
+    let imageUrl : String
+    
+    init(name: String, brand: String, warranty: String, hdmiCount: String, usbCount: String, screenSize: String, audioPower: String, refrshRate: String, screenResolution: String, powerConsumption: String, cost: Int, cpuCoreCount: String, wifiSpecs: String, screenType: String, isSmart: String, hdmiType: String, ciScore: String, rank: Int, imageUrl: String) {
+        self.name = name
+        self.brand = brand
+        self.warranty = warranty
+        self.hdmiCount = hdmiCount
+        self.usbCount = usbCount
+        self.screenSize = screenSize
+        self.audioPower = audioPower
+        self.refrshRate = refrshRate
+        self.screenResolution = screenResolution
+        self.powerConsumption = powerConsumption
+        self.cost = cost
+        self.cpuCoreCount = cpuCoreCount
+        self.wifiSpecs = wifiSpecs
+        self.screenType = screenType
+        self.isSmart = isSmart
+        self.hdmiType = hdmiType
+        self.ciScore = ciScore
+        self.rank = rank
+        self.imageUrl = imageUrl
+    }
+    
+    
+    
 }
